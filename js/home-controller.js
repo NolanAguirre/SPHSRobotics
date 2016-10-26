5c416c6da1ebd9a53b5302200efd707ae3b22c64
nolan aguirre <nolanaguirre08@gmail.com>
2016-10-26 10:00:50 -0500

    remove unused controller

--- js/home-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('SPHSRobotics')
-    .controller('HomeController', HomeController);
-
-HomeController.$inject = ['$routeParams']
-function HomeController($routeParams) {
-    var vm = this;
-}
